abe9a7c9b6191dfabc7a10eb995c0e7238fb6c42
Fortygin <slavafortygin785@gmail.com>
2020-05-25 00:23:11 +0500

    add zero task

---
+++ massive3ver/massive3ver/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace massive3ver
+{
+    class Program
+    {
+        static void IntArray(int[] arr)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            
+            Console.ReadLine();
+        }
+    }
+}
+
+    
+
